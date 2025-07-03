c85791d4c2bb6542ad4c018d05c5f16b1ee68198
Nikhil <nikhil@example.com>
2025-07-03 13:04:46 +0530

    Initial frontend push

--- server/server.js
+++ server/server.js
@@ -1,20 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 // ✅ Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
 // ✅ MongoDB Connection
-mongoose.connect("mongodb+srv://knikhil6293:D16ABAo7LnmGtpwY@cluster0.kpggf7v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('✅ MongoDB connected'))
 .catch((err) => console.error('❌ MongoDB connection error:', err));
 
